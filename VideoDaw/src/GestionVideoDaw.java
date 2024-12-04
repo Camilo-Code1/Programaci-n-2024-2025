8117a95d1885e95e3aeb498f90899f18355f9f41
COMPU-S025\daw1 <camiloarone@gmail.com>
2024-12-04 09:54:44 +0100

    04_12_2024 (Commit Miutilsasdb)

--- VideoDaw/src/GestionVideoDaw.java
+++ VideoDaw/src/GestionVideoDaw.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class GestionVideoDaw {
-    private static Cliente[] clientes = new Cliente[100];
-    private static int numClientes = 0;
 
-    private static Pelicula[] Pelis = new Pelicula[100];
-    private static int numPelis = 0;
 
     public static void main(String[] args) {
+
+        VideoDaw videoClub = null;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a VideoDaw.");
         System.out.println("(Presione enter para continuar)");
         sc.nextLine(); 
 
-        String DNI = obtenerDNI(sc);
+        String DNI = videoClub.obtenerDNI(sc);
         System.out.println("Por favor introduzca su nombre completo: ");
         String nombre = sc.nextLine();
 
@@ -42,13 +41,13 @@ public class GestionVideoDaw {
                     // Implementar 
                     break;
                 case 2:
-                    AgregarPelicula(sc);
+                    videoClub.AgregarPelicula(sc);
                     break;
                 case 3:
-                    AgregarCliente(sc);
+                    videoClub.AgregarCliente(sc);
                     break;
                 case 4:
-                    AlquilerPelicula(sc);
+                    videoClub.AlquilerPelicula(sc);
                     break;
                 case 5:
                     // Implementar
@@ -69,88 +68,5 @@ public class GestionVideoDaw {
 
         sc.close(); 
     }
-
-    private static String obtenerDNI(Scanner scanner) {
-        String DNI;
-        while (true) {
-            System.out.println("Ingrese el DNI de la cuenta (Formato: 00000000X): ");
-            DNI = scanner.nextLine();
-            if (validarDNI(DNI)) {
-                return DNI;
-            }
-            System.out.println("DNI inválido. Por favor, intentelo de nuevo\n");
-        }
-    }
-
-    private static boolean validarDNI(String DNI) {
-        return DNI.matches("\\d{8}[A-Z]{1}");
-    }
-
-    // Opcion 3 Crear y registrar cliente en el videoclub
-    public static void AgregarCliente(Scanner sc) {
-        System.out.println("Ingrese el nombre del cliente: ");
-        String nombre = sc.nextLine();
-        System.out.println("Ingrese el DNI del cliente: ");
-        String DNI = sc.nextLine();
-        System.out.println("Ingrese el número de socio: ");
-        String numSocio = sc.nextLine();
-        System.out.println("Ingrese la dirección del cliente: ");
-        String direccion = sc.nextLine();
-        System.out.println("Ingrese la fecha de nacimiento del cliente: ");
-        String fechaNacimiento = sc.nextLine();
-        System.out.println("Ingrese la fecha de baja del cliente (si aplica): ");
-        String fechaBaja = sc.nextLine();
-        System.out.println("Ingrese el número de películas alquiladas: ");
-        int peliculasAlquiladas = sc.nextInt();
-        sc.nextLine(); 
-
-        Cliente nuevoCliente = new Cliente(DNI, nombre, numSocio, direccion, peliculasAlquiladas);
-
-        if (numClientes < 100) {
-            clientes[numClientes] = nuevoCliente;
-            numClientes++;
-            System.out.println("Cliente agregado con éxito");
-            nuevoCliente.MostrarinfoClientes();
-        } else {
-            System.out.println("No hay espacio para agregar más clientes");
-        }
-    }
-
-    public static void AgregarPelicula(Scanner sc) {
-        System.out.println("Ingrese el codigo de la pelicula: ");
-        String COD = sc.nextLine();
-        System.out .println("Inserte el titulo de la pelicula: ");
-        String Titulo = sc.nextLine();
-        System.out.println("Inserte el genero de la pelicula: ");
-        String Genero = sc.nextLine(); // Averiguar como hacer el enum y todo eso
-        System.out.println("Inserte la fecha de registro: ");
-        String FechadeRegistro = sc.nextLine();
-        System.out.println("Inserte la fecha de baja: ");
-        String FechadeBaja = sc.nextLine();
-        System.out.println("Inserte la fecha del ultimo alquiler: ");
-        String FechadeAlquiler = sc.nextLine();
-        System.out.println("Por favor, indique si la pelicula esta alquilada o no: ");
-        String IsAlquilada = sc.nextLine(); // Hacer un if y luego lea la respuesta como un true o false
-
-        Pelicula Pelisstas = new Pelicula(COD, Titulo, null);
-
-        if (numPelis < 100) {
-            Pelis[numPelis] = Pelisstas;
-            numPelis++;
-            System.out.println("Película agregada con éxito.");
-            Pelisstas.mostrarInfoPelicula();
-        } else {
-            System.out.println("No hay espacio para agregar más películas.");
-        }
-    }
-
-    public static void AlquilerPelicula (Scanner sc) {
-
-    for (int i = 0; i < Pelis.length; i++) {
-        System.out.println("Peliculas: " + Pelis[i]);
-    }
-    }
-
-
 
 } // MAIIIIIIINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
