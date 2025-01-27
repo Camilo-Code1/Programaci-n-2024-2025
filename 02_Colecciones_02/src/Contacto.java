290ba36730f78e3faf160adf81687b2daf90acd8
COMPU-S025\daw1 <camiloarone@gmail.com>
2025-01-27 12:47:01 +0100

    27/01/2025 (Secondo commit)

---
+++ 02_Colecciones_02/src/Contacto.java
@@ -0,0 +1,23 @@
+public class Contacto {
+    String nombre;
+    String telefono;
+    String correo;
+
+    public Contacto(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    
+
+
+
+}
