c205e32bb8eb6b87f17461f77675ffae0b8fdd7f
COMPU-S025\daw1 <camiloarone@gmail.com>
2025-02-06 11:35:22 +0100

    06/02/2025

--- 01_Excepciones_01/src/A4.java
+++ 01_Excepciones_01/src/A4.java
@@ -2,17 +2,43 @@ import java.util.Scanner;
 
 public class A4 {
     public static void main(String[] args) {
+
+
+
         Scanner scanner = new Scanner(System.in);
+        A4 A4_Metodos = new A4();
 
+        int valor; 
+
+        System.out.println("Por favor ingrese un valor: ");
+        valor = scanner.nextInt();
+
+        try {
+            verificarPositivo(valor);
+        } catch (ValorInvalidoException e) {
+            System.out.println("Excepcion atrapada: " + e.getMessage());
+        }
+        
+        try {
+            verificarNegativo(valor);
+        } catch (ValorInvalidoException e) {
+            System.out.println("Excepcion atrapada: " + e.getMessage());
+        }
 
     }
-}
 
-class A4_2 {
-    public void imprimePositivo(int valor) {
-        if (valor > 0) {
-            System.out.println(valor);
+    public static void verificarPositivo(int valor) throws ValorInvalidoException {
+        if (valor < 0) {
+            throw new ValorInvalidoException("El " + valor + " no es positivo");
             }
+            System.out.println("El " + valor + " es positivo");
 
     }
+    public static void verificarNegativo(int valor) throws ValorInvalidoException {
+        if (valor > 0) {
+            throw new ValorInvalidoException("El " + valor + " no es negativo");
+            }
+            System.out.println("El " + valor + " es negativo");
+            }
 }
+
